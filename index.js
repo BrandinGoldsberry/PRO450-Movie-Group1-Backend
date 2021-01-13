586dd2ec9635c58d0c7a5d5de100a29c394aad7a
BrandinGoldsberry <bgoldsberry@student.neumont.edu>
2021-01-12 19:12:14 -0700

    added route files and setup index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const axios = require('axios');
+
+//import routes that CRUD users
+const usersRoutes = require("routes/usersRoutes.js");
+//import routes that CRUD reviews
+const reviewsRoutes = require("routes/reviewsRoutes.js");
+//imports routes that serve the front of the website
+const frontendRoutes = require("routes/frontendRoutes.js");
+
+if(process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
+app.use("/", frontendRoutes);
+app.use("/user", usersRoutes);
+app.use("/static/js", express.static('static/js'));
+app.use("/static/css", express.static('static/css'));
+
+app.use(cors());
+app.listen(process.env.PORT || 5001);
